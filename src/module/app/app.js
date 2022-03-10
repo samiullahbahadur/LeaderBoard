1113e257d40add75c8c5c5d41b4317d800cb9ad5
samiullahbahadur <bahadursamiullah@gmail.com>
2022-03-10 21:39:16 +0430

    fix linter error

--- src/module/app/app.js
+++ src/module/app/app.js
@@ -1,23 +1,23 @@
-import * as api from "../game/game.js";
+import * as api from '../game/game.js';
 
 const addToLeaderboard = (data) => {
-  const formtxt = document.querySelector("form > p");
+  const formtxt = document.querySelector('form > p');
   api.setData(data.user, data.score).then((recieved) => {
     formtxt.innerHTML = recieved.result;
-    formtxt.style.color = "#fff";
+    formtxt.style.color = '#fff';
   });
 };
 
 const displayLeaderboard = () => {
-  const scoreListSection = document.querySelector(".scores");
-  scoreListSection.innerHTML = "";
+  const scoreListSection = document.querySelector('.scores');
+  scoreListSection.innerHTML = '';
   api.getData().then((dataList) => {
     if (!dataList) {
       return;
     }
     const arrangedList = dataList.result.sort((a, b) => b.score - a.score);
     arrangedList.forEach((data) => {
-      const li = document.createElement("li");
+      const li = document.createElement('li');
       li.innerHTML = `<div class ="list-items"><p id="one">${data.user}:</p><p id="two">${data.score}</p>`;
       scoreListSection.appendChild(li);
     });
